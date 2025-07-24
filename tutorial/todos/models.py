84c3d6c305163ab81a954cf6764d32c145212928
Alex <Like2968@gmail.com>
2025-07-24 16:37:26 +0800

    feat(models): 新增 File 模型以支持待辦事項的檔案上傳

--- tutorial/todos/models.py
+++ tutorial/todos/models.py
@@ -10,3 +10,11 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+    
+class File(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE, related_name='files')
+    files = models.FileField(upload_to='todo_files/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'File for {self.todo.title}'
